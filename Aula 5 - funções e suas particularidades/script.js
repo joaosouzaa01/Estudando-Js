bc871c19a08225cb2e6fe144669dc0b567004a2c
João Victor <teixeiralopesdesouzaj@gmail.com>
2023-09-25 17:11:37 -0300

    Aula 05

---
+++ Aula 5 - funções e suas particularidades/script.js
@@ -0,0 +1,20 @@
+// FUNÇÕES - FUNCTION
+
+// COMO DECLARAR ?
+// DEVE SER DECLARADA USANDO A PALAVRA FUNCTION SEGUIDA DE "()" E "{}"
+    function funcao() {
+        console.log('mensagem de uma função');
+    }
+
+// CHAMANDO A FUNÇÃO
+    funcao(); 
+
+
+// FUNÇÃO COM PARÂMETROS
+// DEFININDO PARÂMETROS
+    function mensagem(primeiro, segundo) {
+        console.log(primeiro, segundo)
+    }
+
+// CHAMANDO A FUNÇÃO E ATRIBUINDO VALOR AOS PARÂMETROS.
+    mensagem('Tudo certo', 'jovem!'); 
